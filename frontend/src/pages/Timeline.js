6c97149833bdb01c5d8874a43edf119d82a154ae
Marcus Botelho <marcus.itec@gmail.com>
2019-02-09 12:45:19 -0200

    add socket.io-client and create a func for subscribe

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from "../services/api";
+import socket from 'socket.io-client'
 
 import TwitterLogo from "../twitter.svg";
 import "./Timeline.css";
@@ -13,9 +14,22 @@ export default class Timeline extends Component {
   };
 
   async componentDidMount() {
+    this.subscribeToEvents()
     const response = await api.get("tweets");
 
     this.setState({ tweets: response.data });
+  }
+
+  subscribeToEvents = () => {
+    const io = socket('http://localhost:3000')
+
+    io.on('tweet', data => {
+      console.log(data)
+    })
+
+    io.on('like', data => {
+      console.log(data)
+    })
   }
 
   handleInputChange = e => {
